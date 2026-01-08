6543b8ef17cb60a3b58cf8d8837661a5b09b8f71
tmdgusya <dev0jsh@gmail.com>
2026-01-08 22:30:30 +0900

    mission3 complete

--- go-channel-dojo/internal/mission_03/mission.go
+++ go-channel-dojo/internal/mission_03/mission.go
@@ -8,6 +8,7 @@ package mission03
 
 import (
 	"errors"
+	"time"
 )
 
 // MultiSelect demonstrates select with multiple channels.
@@ -17,10 +18,20 @@ import (
 func MultiSelect() (string, error) {
 	// TODO: Implement this function
 	// 1. Create two channels (ch1, ch2)
+	channel1 := make(chan string)
+	channel2 := make(chan string)
 	// 2. Send "from ch1" to ch1 in a goroutine after a short delay
+	go func() {
+		time.Sleep(time.Millisecond)
+		channel1 <- "from ch1"
+	}()
 	// 3. Use select to receive from either channel
-	// 4. Return the received message
-	return "", nil
+	select {
+	case message := <-channel1:
+		return message, nil
+	case message := <-channel2:
+		return message, nil
+	}
 }
 
 // NonBlockingReceive demonstrates non-blocking channel receive.
@@ -28,12 +39,16 @@ func MultiSelect() (string, error) {
 //
 // Returns the message and whether data was received.
 func NonBlockingReceive() (message string, received bool, err error) {
-	// TODO: Implement this function
 	// 1. Create a channel
+	ch := make(chan string)
 	// 2. Use select with default case to attempt receive
-	// 3. If message received, set received=true and message
-	// 4. If default case, set received=false
-	return "", false, nil
+	select {
+	case msg := <-ch:
+		return msg, true, nil
+	default:
+		// 4. If default case, set received=false
+		return "", false, nil
+	}
 }
 
 // NonBlockingSend demonstrates non-blocking channel send.
@@ -41,12 +56,15 @@ func NonBlockingReceive() (message string, received bool, err error) {
 //
 // Returns whether the send succeeded.
 func NonBlockingSend() (sent bool, err error) {
-	// TODO: Implement this function
 	// 1. Create a buffered channel with capacity 1
-	// 2. Send a value (should succeed)
-	// 3. Try to send another value using select with default
-	// 4. Return whether the second send succeeded
-	return false, nil
+	ch := make(chan int, 1)
+	// 2. Send a value using non-blocking select
+	select {
+	case ch <- 1:
+		return true, nil
+	default:
+		return false, nil
+	}
 }
 
 // TimeoutSelect demonstrates timeout with select.
@@ -54,12 +72,21 @@ func NonBlockingSend() (sent bool, err error) {
 //
 // Returns the result message or timeout error.
 func TimeoutSelect() (result string, err error) {
-	// TODO: Implement this function
 	// 1. Create a channel
+	ch := make(chan string)
 	// 2. Start a goroutine that sends after 200ms
+	go func() {
+		time.Sleep(200 * time.Millisecond)
+		ch <- "done"
+	}()
 	// 3. Use select with time.After(100ms) for timeout
-	// 4. Return the message or timeout error
-	return "", errors.New("timeout")
+	select {
+	case result = <-ch:
+		return result, nil
+	case <-time.After(100 * time.Millisecond):
+		// 4. Return the message or timeout error
+		return "", errors.New("timeout")
+	}
 }
 
 // PrioritySelect demonstrates select with prioritized channels.
@@ -67,11 +94,25 @@ func TimeoutSelect() (result string, err error) {
 //
 // Returns the message from the first available channel.
 func PrioritySelect() (string, error) {
-	// TODO: Implement this function
 	// 1. Create two channels (ch1, ch2)
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 	// 2. Send to ch2 immediately (not ch1)
+	go func() {
+		ch2 <- "from ch2"
+	}()
+	time.Sleep(time.Millisecond)
 	// 3. Use nested select or structured approach
 	// 4. First try ch1 (will block), then ch2
-	// 5. Return the received message
-	return "", nil
+	select {
+	case msg := <-ch1:
+		return msg, nil
+	default:
+		select {
+		case msg := <-ch2:
+			return msg, nil
+		default:
+			return "", nil
+		}
+	}
 }
